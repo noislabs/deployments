0235d50157e85ad8f9387dc952e5f1593edfc23d
Kais <baccour.kais@yahoo.fr>
2023-03-29 19:37:54 -0300

    drand-bots

---
+++ allowlisted-bots/nois-1/drand-bot-allowlist.sh
@@ -0,0 +1,67 @@
+allowlist_addresses=(
+"nois1dp4fnnagxdk46vh9nvlg83jml45d4kpmzcxqjl" #alkadeta#2699
+"nois1rzf9y9cfudr0n46vt32kd46x8lwem05xxxy646" #BccNodes#3882
+"nois1sc4rwm6atdeywv6fjj9zn2sj3xfzthvn07ee57" #chillyvee#1883
+"nois1n2ch4szayfecyat9u246r4n9svt7en0hdk9ypc" #theBro#6092
+"nois17ee7c6xkd3c52n7mgfwdgeqy39lrcml75rky2a" #cwt#3491
+"nois1g45c8pkztfnfjfgtyfp73mmc0ppy2qv90mqde2" #notRizbe | Gelotto 🍦#8148
+"nois108egjvry22wtsvjmyhk77j63jncd0tlexns0hn" #jayjay#4970
+"nois1rt9tmq8d09um7f6ac4h2sh7fpfdsthlhcg3tkm" #Kolot#5637
+"nois1mzddued98y2v43autz0uuksh5eg2muhaggjjph" #lesnik_utsa#4480
+"nois162xyu6an7nru8ec2tzjfgx9jhkwetec4jfqudr" #Stake&Relax#8787
+"nois1xrrfek3qpz4ja8ccrjklfy064asyqq9kz6t9ch" #polkachu.com#1249
+"nois1t9z3aa7gk3edhxnpn6akxdmk8nzvh0kukchnum" #usurper#8680
+"nois1ss32pljxfzan36rdwdvn22ugetp5gyurnx0l4d" #BukTaPaBu4#9154
+"nois1s7ll723zu6j0utf33zsdwlzl26j4mnhsexwlkg" #Voynitskiy | AlxVoy#7896
+"nois1em0rgk80hmvel9vlaqthrvu9avfp0kgwajq398" #kivanc | Coin Stamp#4854
+"nois1x2njezxg2sszvtmm77v6c3s9yyy3m44jdpclvt" #Mon | cumulo.pro#7662
+"nois175lcweer8hwvd2w9nt66swc066nd40vz3adfcu" #donadel#7181
+"nois1xa8a3a5ugjgrpydupasg7ynyj04xfw5m4jf54f" #Tom | ECO Stake 🌱#1505
+"nois1sx6xt3xvdaq8swuaqrua8c63nwfhn0edmltzyr" #SEM#4095
+"nois1n72me4fn3ex7j3ca9p9pwnd4emzqddfhqyvr3l" #Jaha#9160
+"nois1ktv6sh59ahujzw2r3vckhutvaf68un2a0x3428" #JamesBrandon#4096
+"nois1tevuzecs5t03jny3lxcre5dvde4e7xy0dykwq7" #kjnodes#8455
+"nois14qekdkj2nmmwea4ufg9n002a3pud23y852apag" #schultzie | Lavender.Five Nodes#3231
+"nois1lagqlmjykjthpp30ft7w7fp9rcjx0f76zkt3gg" #mahof#6504
+"nois12090t6r7kx87hxdh3z3yqu0vys96h62sp69txe" #mcb2001#6827
+"nois1nl6pnh3vy6pptxauhtk5wuv9625hxtdv6s7yht" #narkosha#0808
+"nois1u3284r37jq0tyd4rynz35k0kk23q3hvynr8c5c" #! PPNV Service !#0083
+"nois1cdqhp3ecnyzwjqp4pshxl8a6xy6vl2trwkwgyr" #rjsimp#1081
+"nois1sj2zl4d2h0jzwsvs5enfrjwk0uhkcz8ew9w3yv" #SAKURA#0129
+"nois190upwptd9vsmcmla5u84uznk6p02dh2eddl8m8" #! SECARD#7408
+"nois14mxn9ajv5y9swfgdzggsktgayt7c49gknzr5v3" #Adorid | SGTstake#1293
+"nois1n766g9ya69q922pwu085yf0qr5562hv4279j6f" #Shoni#5605
+"nois1f2lt97lta23agdd4hpgvd4n7yh4008d9u4c44f" #silent666#8303
+"nois1lh76lsp4rpzhw9lcwkxsfegpu9qqf332z5zthy" #sr20de#7650
+"nois1r6sua9u3h60kxmdgeq9ehx84z0gxw5c9tfn3g8" #stakr.space#7636
+"nois1r68ncs0fa4cmf7g2yj922cd057yf6jp0x90m5t" #Mikhail#5221
+"nois10m7ykheru45swv6uqm86q9ft5ltt57rf2jhknj" #vitek#9360
+"nois1w2zwhuafevcnutuweyhqm8vv2llkmmhyp8rjk3" #CreeptoCat#8121
+"nois1a2chy0mavx80406z9tncyjcqq4v2zwzhjrwerj" #AndreasVorobey#3014
+"nois1vtzmulynjprfgure67k6j9dav5m25w7kw589s9" #AlexeyM#5409
+"nois1cnkyzyr2yywgflx53ay4r90fxgu8kx6s2ug039" #arda | Validator.run#8670
+"nois10hcnj06d32khtknfgxkcs05amnw4wrl8w23c8m" #NakoTurk#5452
+"nois1g5h25dmug042jc93p6rk00ssnr0ql3kqn8kvlc" #tsigor#5666
+"nois10rwh09k3y7xdxq54mhnan0gqpjw4ysplx6j76j" #HamzaK#5652
+"nois1522y2vxy6kymt6m8zxvum8x44fy9suhyav2m4q" #AlexHuk#8647
+"nois1q5yc2w6w8hrtxdg7gdddc9wz7medcn2l5f7jrz" #Zak | EZStaking.io | FR#4931
+"nois1z65emyfm596r75p5wdhuflacn3tjldqly5jpc6" #ggbb#2026
+"nois1kdyqakykk24xhhpzzaqwzmlz57j0w4769pjwhh" #TWan#9414
+"nois1ga2zq65vz8kweghfkwxjuxxp3pusdp8v4q59jr" #aidil.sol#4258
+"nois1rjrhem49wz2l8d39x92uz2l9gsp0nr7anx3xg2" #cagie#7144
+"nois1d0p8273lg6nfn98wn73ag9k3t5qkceqdtmjw0l" #GArry#6418
+"nois1mxr3ked2epa5fsa66hr3uut9wz524whjq28js5" #NGINE | AutoStake#7943
+"nois178dv4grac5d9q7ppm7lssl3z50t0974vrqy8sk" #cyberG#4889
+"nois1nrcu9wc7yufqsu6t6ve0rejx4eq6tyspwxqdsx" #farukyasar#9261
+"nois1zefr9ncfpyjm3af6g7e4sxf3ywmlecyjtedtsp" #haciyatmaz#0312
+"nois14ty5h7dhnses6smx5zwck5kh4j27rrgwwtft77" #hexskrt.eth#7637
+"nois10j4c7utvk7frzdp4rxpjd2w5l9rgtyaaaz7dcn" #Indonode#5255
+"nois1f2qxqj427vewglwv2788yqeq7kuk0hwphc5cux" #MarkCN#6867
+"nois1luknjhnmcarptur4dcuvhkd9cw92x2u92l7tzt" #FOTON | Maksim#0733
+"nois1k5wvu89mxwthx4kt630p5hc66we5vfwdxph9q2" #i legenda#8021
+"nois1dqwc7l8d3y32p2taasdpeskg0myyfl2jkn5lqu" #shurinoff#5643
+"nois16c2zwj8dr5zzh782xuqtft259dc9lqtp0n3uws" #Northtrend#6436
+"nois183zpsxgc0v5fyadg2pxlz5pr0dn64w30uu549s" #Robsberry#4903
+"nois12qvnsdlnq95j2dcwc9vk77etezfsk3awgvar60" #RuthWright#2752
+"nois10gzkvkj5q23q5gpk47kftwjvxqhaerxfh2984s" #thanhbinhpc2007#7611
+)
---
+++ allowlisted-bots/nois-1/sync-contract.sh
@@ -0,0 +1,20 @@
+set -ex
+source drand-bot-allowlist.sh
+source drand-bot-removelist.sh
+
+NOIS_DRAND_CONTRACT=nois19w26q6n44xqepduudfz2xls4pc5lltpn6rxu34g0jshxu3rdujzsj7dgu8
+
+noisd tx wasm execute $NOIS_DRAND_CONTRACT \
+'{"update_allowlist_bots": {
+    "add": ["'$(echo ${allowlist_addresses[@]} \
+        | tr -d '"' \
+        | sed 's/ /","/g')'"],"remove": ['$(echo ${remove_addresses[@]} \
+        | tr -d '"' \
+        | sed 's/ /","/g')']}}' \
+--from deployment-key \
+--chain-id nois-1 \
+--gas=auto \
+--gas-adjustment 1.3 \
+--gas-prices=0.05unois \
+--broadcast-mode=block \
+--node=https://nois-mainnet-rpc.bccnodes.com:443
