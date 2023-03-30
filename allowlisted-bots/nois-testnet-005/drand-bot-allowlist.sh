b41613d858e6da2ac6f8ac1500ad8339146ad546
Kolot <104339267+Kolot86@users.noreply.github.com>
2023-03-30 20:02:48 +0200

    Update drand-bot-allowlist.sh

--- allowlisted-bots/nois-testnet-005/drand-bot-allowlist.sh
+++ allowlisted-bots/nois-testnet-005/drand-bot-allowlist.sh
@@ -2,6 +2,6 @@ allowlist_addresses=(
 "nois1ewsargrw5jfywzplpj6tj8r7fmsscn8ydqnfye" #SEM#4095
 )
 allowlist_addresses=(
-"nois1kqpvdzlrmnmw78fyl34twlsx3jy2yptnfcatnc" Kolot#5637
+"nois1kqpvdzlrmnmw78fyl34twlsx3jy2yptnfcatnc" #Kolot#5637
 )
 
