7ac0165fbc5aaf21c331a45e9eee7ed8bcb9853f
Sr20de <106104431+Sr20dem@users.noreply.github.com>
2023-03-30 22:02:37 +0300

    add Sr20de bot address for nois-testnet-005

--- allowlisted-bots/nois-testnet-005/drand-bot-allowlist.sh
+++ allowlisted-bots/nois-testnet-005/drand-bot-allowlist.sh
@@ -1,4 +1,5 @@
 allowlist_addresses=(
 "nois1ewsargrw5jfywzplpj6tj8r7fmsscn8ydqnfye" #SEM#4095
 "nois1kqpvdzlrmnmw78fyl34twlsx3jy2yptnfcatnc" #Kolot#5637
+"nois1c359j2vhgvjsvq92ew6mvjkt2ww4q3k9rrz2ka" #sr20de#7650
 )
