c84802af08e64e82a92d710b1f50298bf0e3ca1d
alkadeta <72661026+alkadeta@users.noreply.github.com>
2023-03-31 00:57:05 +0300

    add alkadeta for testnet drand

--- allowlisted-bots/nois-testnet-005/drand-bot-allowlist.sh
+++ allowlisted-bots/nois-testnet-005/drand-bot-allowlist.sh
@@ -2,4 +2,5 @@ allowlist_addresses=(
 "nois1ewsargrw5jfywzplpj6tj8r7fmsscn8ydqnfye" #SEM#4095
 "nois1kqpvdzlrmnmw78fyl34twlsx3jy2yptnfcatnc" #Kolot#5637
 "nois1c359j2vhgvjsvq92ew6mvjkt2ww4q3k9rrz2ka" #sr20de#7650
+"nois1dp4fnnagxdk46vh9nvlg83jml45d4kpmzcxqjl" #alkadeta#2699
 )
