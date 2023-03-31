1fc340d0ef7c5d64539bdae2b9300a48128b9829
Simon Warta <2603011+webmaster128@users.noreply.github.com>
2023-03-31 13:15:43 +0200

    Add jayjay testnet bot

--- allowlisted-bots/nois-testnet-005/drand-bot-allowlist.sh
+++ allowlisted-bots/nois-testnet-005/drand-bot-allowlist.sh
@@ -4,4 +4,5 @@ allowlist_addresses=(
 "nois1c359j2vhgvjsvq92ew6mvjkt2ww4q3k9rrz2ka" #sr20de#7650
 "nois1dp4fnnagxdk46vh9nvlg83jml45d4kpmzcxqjl" #alkadeta#2699
 "nois19t6kxd4mmkawxtmejxjt3x597xtdevufxug0ma" #narkosha#0808
+"nois108egjvry22wtsvjmyhk77j63jncd0tlexns0hn" #jayjay#4970
 )
