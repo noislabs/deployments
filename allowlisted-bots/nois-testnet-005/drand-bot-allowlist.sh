4692854178ef769872ab4d32d53df3dacdc713f2
Moncho Santos <moncho.santosdios@gmail.com>
2023-04-07 22:20:57 +0200

    Update drand-bot-allowlist.sh

--- allowlisted-bots/nois-testnet-005/drand-bot-allowlist.sh
+++ allowlisted-bots/nois-testnet-005/drand-bot-allowlist.sh
@@ -8,4 +8,5 @@ allowlist_addresses=(
 "nois108egjvry22wtsvjmyhk77j63jncd0tlexns0hn" #jayjay#4970
 "nois1xlakn970hafnal4fjp7r8j4fy9ulkcct4m7z0v" #manueldb | Stake&Relax#8787
 "nois1zefr9ncfpyjm3af6g7e4sxf3ywmlecyjtedtsp" #haciyatmaz#0312
+"nois1e5lfc5jm3fal0hv4ra3evnq6v96ff2wk6tqsuy" #Mon | cumulo.pro#7662
 )
