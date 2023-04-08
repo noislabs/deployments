739e89e1e0578a0eb6ed971a260b5ab4d2b6ff7a
jcdmacleod <jcdmacleod@gmail.com>
2023-04-08 16:31:12 +0200

    Update allowlist - Add Consensus One

--- allowlisted-bots/nois-1/drand-bot-allowlist.sh
+++ allowlisted-bots/nois-1/drand-bot-allowlist.sh
@@ -67,4 +67,5 @@ allowlist_addresses=(
 "nois15tugy997tfc2en35npkrtmaw7vjn0adqva64a2" #orahapris#4610
 "nois18e6lpf6euuc5rwu409ev2xrey8jchnx2qjypcr" #terlia_nat#1620
 "nois1ka67nryekpgtzret6dy8cc476nzg0x0t89ezgc" #linamr#0394
+"nois170kndajnnvyz0hwwd6h50upt0u86mmuks883wu" #John Mac#9977
 )
